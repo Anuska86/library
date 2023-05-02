4f5263196caf5a2df9624603c372a39e3095e8ca
Anuska86 <anasrm86@gmail.com>
2023-05-02 19:23:33 +0200

    Basics

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -290,3 +290,18 @@ function validatestock() {
   }
   return isValid;
 }
+
+function tempAlert(msg,duration)
+{
+     var el = document.createElement("div");
+     el.setAttribute("style","position:absolute;top:40%;left:20%;background-color:white;");
+     el.innerHTML = msg;
+     el.style.background = "green";
+     setTimeout(function(){
+      el.parentNode.removeChild(el);
+     },duration);
+     document.body.appendChild(el);
+}
+
+var d = document.getElementById('modalButton');
+d.onclick = function(){ tempAlert("Success!!",2000); };
